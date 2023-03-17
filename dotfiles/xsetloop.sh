db8177df4dea1c36f7101aff0dfbfc1b10009031
Sean <informatikagdsb@proton.me>
2023-03-17 11:48:55 +0100

    Update xsetloop.sh-added symbols and more
    better functionality, nerd font symbols and better status bar overall

--- dotfiles/xsetloop.sh
+++ dotfiles/xsetloop.sh
@@ -1,31 +1,42 @@
-#!/bin/bash
-status() {
-    # Volume %
-    echo "V:"$(amixer sget Master | grep -o "[0-9]*%\|\[on\]\|\[off\]")
+#!/usr/bin/env bash
 
-    # Battery %
-    echo "B:"$(acpi -b | grep -o '[0-9]*%')
+# Change this depending on your battery in /sys/class/power_supply/
+battery="BAT0";
 
-    # Current keyboard layout
-
-    layoutValue=$(xset -q | grep LED | awk '{ print $10 }')
-
-    case $layoutValue in
-	00000000|00000001) layout="en" ;;
-#	00000001) layout="EN" ;;
-	00001000|00001001) layout="fr" ;;
-#	00001001) layout="FR" ;;
-	*) layout = "??" ;;
-    esac
-
-    echo $layout
-
-    # Date & time
-    date '+%Y %a %d %b %I:%M%p'
-    
+has_battery() {
+    if [ -d /sys/class/power_supply/$battery ]; then
+        return 0;
+    fi
+    return 1;
+}
+get_battery_status() {
+    charge="$(get_charge)"
+    echo ""$icon" "$charge"%"
 }
 
-while :; do
-    xsetroot -name "$(status | tr '\n' ' ')"
-    sleep 1m
+get_charging_status() {
+    cat "/sys/class/power_supply/$battery/status"
+}
+
+get_charge() {
+    cat "/sys/class/power_supply/$battery/capacity"
+}
+
+get_datetime() {
+    date +"󰃭 %a %d %b %Y | 󱑕 %I:%M %p %Z"
+}
+
+get_status() {
+    battery_status="";
+    if $(has_battery); then
+        battery_status=" $(get_battery_status) |";
+    fi
+
+    echo "${battery_status} $(get_datetime)";
+}
+
+while true
+do
+    xsetroot -name "$(get_status)";
+    sleep 1m;
 done
